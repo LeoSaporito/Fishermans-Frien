54dff1527b011f0a4f979b95a339539fb75a360c
Leo21897041 <leosaporito16@gmail.com>
2026-05-03 16:09:32 -0400

    Added tilemap and player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public enum MovementState
+    { 
+        Idle,
+        MovingVertical,
+        MovingHorizontal,
+    }
+
+    public MovementState currentState;
+
+    public float moveSpeed;
+    
+    public Vector2 directionalInput;
+
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        switch (currentState)
+        { 
+            case MovementState.MovingHorizontal:
+            rb.linearVelocity = new Vector2(moveSpeed * directionalInput.x, 0);
+            break;
+        case MovementState.MovingVertical:
+            rb.linearVelocity = new Vector2(0, moveSpeed * directionalInput.y);
+            break;
+        case MovementState.Idle:
+            rb.linearVelocity = Vector2.zero;
+            break;
+        }
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        directionalInput = context.ReadValue<Vector2>();
+
+        if (directionalInput.x != 0)
+        {
+            currentState = MovementState.MovingHorizontal;
+            directionalInput = new Vector2(Mathf.Sign(directionalInput.x), 0);
+        }
+        else if (directionalInput.y != 0)
+        {
+            currentState = MovementState.MovingVertical;
+            directionalInput = new Vector2(0, Mathf.Sign(directionalInput.y));
+        }
+        else
+        { 
+            currentState = MovementState.Idle;
+        }
+    }
+}
